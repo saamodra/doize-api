f012358f72f269772b2b95bac4ccb212d13653d2
Rizki Kurniawati <rzkurniawati.s@gmail.com>
2021-07-12 22:33:05 +0700

    Create Assignment.php

---
+++ app/Models/Assignment.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model {
+    protected $table = 'assignment';
+
+    protected $primaryKey = 'id_assignment';
+
+    protected $fillable = [
+        'name_assignment', 'description_assignment', 'duedate_assignment', 'reminder_at', 'priority', 'priority', 'working_status', 'status', 'id_user', 'creadate', 'modidate'
+    ];
+
+    public $timestamps = false;
+
+    /**
+     * Get the user that owns the dailyactivity.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+}
