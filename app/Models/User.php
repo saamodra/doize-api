ca74f05ec014465fdcc4187f5d001eb964be6a06
Rizki Kurniawati <rzkurniawati.s@gmail.com>
2021-08-06 17:16:11 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,9 +31,6 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      *
      * @var array
      */
-    protected $hidden = [
-        'password',
-    ];
 
     public $timestamps = false;
 
